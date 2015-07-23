d9272453ef603c48ce27c1f0a1d6080d14ea1b9a
vistaxpman <lenon_vn3000@yahoo.com>
2015-07-23 22:27:10 +0700

    Update page-blog-1.php

--- working/page-blog-1.php
+++ working/page-blog-1.php
@@ -4,172 +4,115 @@
 
 <div id="wrapper">
 
-<div class="content-wrapper clear">
+    <div class="content-wrapper clear">
 
-<!-- START HEADER -->
+        <!-- START HEADER -->
 
-<div id="header-wrapper">
+        <div id="header-wrapper">
 
-    <div class="header clear">
+            <div class="header clear">
 
-        <div id="logo">
-            <a href="index.html"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png" alt="" /></a>
-        </div><!--END LOGO-->
+                <div id="logo">
+                    <a href="index.html"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png" alt="" /></a>
+                </div><!--END LOGO-->
 
-        <?php
-        $args = array(
-            'container_id'     => 'primary-menu',
-            'container'        => 'div',
-            'menu'             => 'Primary Menu',
-            'menu_class'       => 'menu',
-        );
-        wp_nav_menu($args);
-        ?>
+                <?php
+                $args = array(
+                    'container_id'     => 'primary-menu',
+                    'container'        => 'div',
+                    'menu'             => 'Primary Menu',
+                    'menu_class'       => 'menu',
+                    'theme_location'    => 'primary'
+                );
+                wp_nav_menu($args);
+                ?>
 
-    </div><!--END HEADER-->
+            </div><!--END HEADER-->
 
-</div><!--END HEADER-WRAPPER-->
+        </div><!--END HEADER-WRAPPER-->
 
-<!-- END HEADER -->
+        <!-- END HEADER -->
 
-<div class="section-title">
+        <div class="section-title">
 
-    <h1 class="title">Blog <span>you see kids, once upon a time</span></h1>
+            <h1 class="title">Blog <span>you see kids, once upon a time</span></h1>
 
-</div><!--END SECTION TITLE-->
+        </div><!--END SECTION TITLE-->
 
-<div id="inner-content" class="blog1">
+        <div id="inner-content" class="blog1">
+            <?php
+            $argsPost = array(
+                'post_type'         => 'post',
+                'orderby'           => 'title',
+                'order'             => 'ASC',
+                'posts_per_page'    => 10,
+            );
+            $query = new WP_Query( $argsPost );
+            ?>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="post">
-            <div class="post-info">
-                <div class="date"><span class="month"><?php the_time( 'M' ); ?></span><span class="day"><?php the_time( 'd' ); ?></span><span class="month"><?php the_time( 'Y' ); ?></span></div>
-                <div class="comments"><?php comments_popup_link( 'No comments »', '1 comment', '% comments' ); ?></div>
-            </div><!--END POST-INFO-->
+            <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+            <div class="post">
+                <div class="post-info">
+                    <div class="date"><span class="month"><?php the_time( 'M' ); ?></span><span class="day"><?php the_time( 'd' ); ?></span><span class="month"><?php the_time( 'Y' ); ?></span></div>
+                    <div class="comments">
 
-            <div class="post-content">
+                        <?php comments_number('<a href="javascript:void(0)"><span>0</span> comments</a>', '<a href="javascript:void(0)"><span>1</span> comment</a>', '<a href="javascript:void(0)"><span>%</span> comments</a>'); ?>
+                    </div>
+                </div><!--END POST-INFO-->
 
-                <div class="post-media">
+                <div class="post-content">
 
-                <!-- Do it later -->
-                <div class="flexslider" id="blog-slider">
-                    <ul class="slides">
-                        <li>
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/slider4.jpg" alt="" />
-                            <p class="flex-caption">This is a place for a brief description.</p>
-                        </li>
-                        <li>
-                            <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/slider2.jpg" alt="" /></a>
-                            <p class="flex-caption">Here we go again, history repeating!</p>
-                        </li>
-                        <li>
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/slider1.jpg" alt="" />
-                        </li>
-                    </ul><!--END UL SLIDES-->
-                </div><!--END FLEXSLIDER-->
+                    <div class="post-media">
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                    </div><!--END POST-MEDIA-->
 
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                </div><!--END POST-MEDIA-->
+                    <div class="post-title">
+                        <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    </div><!--END POST-TITLE-->
 
-                <div class="post-title">
-                    <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                </div><!--END POST-TITLE-->
+                    <div class="post-meta">
+                        <ul>
+                            <li><span>Posted by</span> <?php the_author_posts_link(); ?></li>
+                            <li><span>in</span> <?php the_category(', '); ?></li>
+                        </ul>
+                    </div><!--END POST-META-->
 
-                <div class="post-meta">
-                    <ul>
-                        <li><span>Posted by</span> <?php the_author_posts_link(); ?></li>
-                        <li><span>in</span> <?php the_category(', '); ?></li>
-                    </ul>
-                </div><!--END POST-META-->
+                    <?php the_excerpt(); ?>
+                    <p><a href="<?php the_permalink(); ?>" class="more-link">Continue reading &rarr;</a></p>
 
-                <?php the_excerpt(); ?>
-                <p><a href="<?php the_permalink(); ?>" class="more-link">Continue reading &rarr;</a></p>
+                </div><!--END POST-CONTENT -->
+            </div><!--END POST-->
+            <?php endwhile; else : ?>
+            <p> <?php echo _e( 'Sorry, no posts matched your criteria.' ); ?> </p>
+            <?php endif;?>
+            <!-- show paging
+            <div class="wp-pagenavi">
+                <span class="pages">Page 1 of 9</span>
+                <span class="current">1</span>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">&rarr;</a>
+            </div>
+            -->
+            <?php if (function_exists("pagination")) {
+                pagination($query->max_num_pages);
+            } ?>
+            <!--END WP-PAGENAVI-->
 
-            </div><!--END POST-CONTENT -->
-        </div><!--END POST-->
-    <?php endwhile; else : ?>
-        <p> <?php echo _e( 'Sorry, no posts matched your criteria.' ); ?> </p>
-    <?php endif;?>
-    <!-- show paging -->
-    <div class="wp-pagenavi">
-        <span class="pages">Page 1 of 9</span>
-        <span class="current">1</span>
-        <a href="#">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">&rarr;</a>
-    </div><!--END WP-PAGENAVI-->
-
-</div><!--END INNER-CONTENT-->
-
+        </div><!--END INNER-CONTENT-->
 
 
-<div id="sidebar">
 
-    <div class="widget">
-        <h3 class="title">Popular posts</h3>
+        <div id="sidebar">
 
-        <ul class="bra_recent_entries">
-            <li>
-                <span class="date">June 29, 2012</span>
-                <a href="#">Erat nisl faucibus justo verit et quasi architecto beatae.</a>
-            </li>
+            <?php get_sidebar(); ?>
 
-            <li>
-                <span class="date">June 23, 2012</span>
-                <a href="#">Lorem ipsum dolor sit amet consectetur.</a>
-            </li>
+        </div><!--END SIDEBAR-->
+        <?php get_footer(); ?>
 
-            <li>
-                <span class="date">May 19, 2012</span>
-                <a href="#">Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</a>
-            </li>
-
-            <li>
-                <span class="date">April 12, 2012</span>
-                <a href="#">Mattis purus sit amet fermentum.</a>
-            </li>
-        </ul><!--END UL-->
-    </div><!--END WIDGET-->
-
-    <div class="widget">
-        <h3 class="title">Recent comments</h3>
-
-        <ul id="recentcomments">
-            <li>
-                <a href="#" class="author">Jane Doe</a> on <a href="#">Lorem ipsum dolor sit amet consectetur.</a>
-            </li>
-
-            <li>
-                <a href="#" class="author">John Doe Junior</a> on <a href="#">Mattis purus sit amet fermentum.</a>
-            </li>
-
-            <li>
-                <a href="#" class="author">Jane Doe</a> on <a href="#">Erat nisl faucibus justo verit et quasi architecto beatae.</a>
-            </li>
-
-            <li>
-                <a href="#" class="author">John Doe Senior</a> on <a href="#">Erat nisl faucibus justo verit et quasi architecto beatae.</a>
-            </li>
-        </ul><!--END UL-->
-    </div><!--END WIDGET-->
-
-    <div class="widget">
-        <h3 class="title">Archive</h3>
-        <ul class="archive">
-            <li><a href="#">February 2012</a>&nbsp;(2)</li>
-            <li><a href="#">March 2012</a>&nbsp;(1)</li>
-            <li><a href="#">April 2012</a>&nbsp;(5)</li>
-            <li><a href="#">May 2012</a>&nbsp;(4)</li>
-            <li><a href="#">June 2012</a>&nbsp;(3)</li>
-        </ul><!--END UL-->
-    </div><!--END WIDGET-->
-
-</div><!--END SIDEBAR-->
-<?php get_footer(); ?>
-
-</div><!-- END CONTENT-WRAPPER -->
+    </div><!-- END CONTENT-WRAPPER -->
 
 </div><!-- END WRAPPER -->
 
